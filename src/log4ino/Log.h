ee0bdba6591688f19de911b40e3d6cab8d239ff6
Mauricio Jost <mauricio.jost@gmail.com>
2017-02-26 21:44:36 +0100

    Update license

--- src/log4ino/Log.h
+++ src/log4ino/Log.h
@@ -1,23 +1,23 @@
 /*
- * ZINO: low-budget autonomous plant care system.
+ * LOG4INO
  *
  * Copyright 2016 Mauricio Jost
  * Contact: mauriciojostx@gmail.com
  *
- * This file is part of ZINO.
+ * This file is part of LOG4INO.
  *
- * ZINO is free software: you can redistribute it and/or modify
+ * LOG4INO is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * ZINO is distributed in the hope that it will be useful,
+ * LOG4INO is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ZINO. If not, see <http://www.gnu.org/licenses/>.
+ * along with LOG4INO. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LOG_INC
